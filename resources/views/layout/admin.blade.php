7249fff70116ff9d2166165d535247f65b6a163f
ryan21rb <baktiryan182@gmail.com>
2024-02-19 15:15:00 +0700

    icon daus

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('template/assets/css/main/app.css') }}">
     <link rel="stylesheet" href="{{ asset('template/assets/css/main/app-dark.css') }}">
     <link rel="shortcut icon" href="{{ asset('template/assets/images/logo/favicon.svg') }}" type="image/x-icon">
@@ -55,12 +56,15 @@
                     <i class="bi bi-stack"></i>
                     <span>Components</span>
                 </a>
-                <ul class="submenu ">
-                    <li class="submenu-item ">
-                        <a href="">Data User</a>
+                <ul class="submenu">
+                    <li class="submenu-item">
+                        <a href="#">
+                            <i class="fas fa-user"></i>
+                            <span>Data User</span>
+                        </a>
                     </li>
                 </ul>
-            </li>
+                
             
             
             {{-- <li class="sidebar-title">Forms &amp; Tables</li>
