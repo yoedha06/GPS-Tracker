12dcb17a6eba5b6aead07450cd9b74c255146112
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-02-20 17:01:52 +0700

    nice

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -113,6 +113,12 @@
                             </li>
                         </ul>
                     </li>
+                    <li class="sidebar-item">
+                        <a href="" class='sidebar-link'>
+                            <i class="bi bi-box-arrow-left"></i>
+                            <span>Logout</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
