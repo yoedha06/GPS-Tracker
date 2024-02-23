f0be4a85d2fa9b376a0d0c7690a0606c87237179
ryan21rb <baktiryan182@gmail.com>
2024-02-23 14:11:03 +0700

    p

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,11 +1,25 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GEEX</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('template/assets/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/assets/css/main/app-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/assets/css/shared/iconly.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
-@section('content')
+    <link href="/images/geex.png" rel="icon">
+    <link rel="stylesheet" href="{{ asset('template/assets/css/shared/iconly.css') }}">
+</head>
+<body>
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mt-5">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card p-4" style="border-radius: 10px;">
+                    <div class="card-header text-center" style="font-size: 24px; background-color: #f8f9fa;">{{ __('Verify Your Email Address') }}</div>
 
                     <div class="card-body">
                         @if (session('resent'))
@@ -14,12 +28,12 @@
                             </div>
                         @endif
 
-                        {{ __('Before proceeding, please check your email for a verification link.') }}
-                        {{ __('If you did not receive the email') }},
+                        <p style="font-size: 18px;">{{ __('Before proceeding, please check your email for a verification link.') }}</p>
+                        <p style="font-size: 18px;">{{ __('If you did not receive the email') }},</p>
                         <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                             @csrf
                             <button type="submit"
-                                class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                                class="btn btn-link p-0 m-0 align-baseline" style="font-size: 18px;">{{ __('click here to request another') }}</button>.
                         </form>
 
                     </div>
@@ -27,4 +41,12 @@
             </div>
         </div>
     </div>
-@endsection
+
+    <script src="{{ asset('template/assets/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('template/assets/js/app.js') }}"></script>
+
+    <!-- Need: Apexcharts -->
+    <script src="{{ asset('template/assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/pages/dashboard.js') }}"></script>
+</body>
+</html>
