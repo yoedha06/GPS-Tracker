016a1abdd5eb31072b88a8ab423c0ea35d785e1f
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-02-23 13:21:00 +0700

    Remove update email profile

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\LoginController as AuthLoginController;
 use App\Http\Controllers\Auth\RegisterController as AuthRegisterController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MapController;
@@ -62,7 +63,7 @@ Route::middleware(['verified', 'auth', 'role:customer'])->group(function () {
     Route::get('/customer', [TampilanController::class, 'index'])->name('index.customer');
     Route::get('/customer/profile', [ProfileController::class, 'index'])->name('customer.profile');
     Route::get('/history/customer', [HistoryController::class, 'index'])->name('customer.history.index');
-    Route::put('/customer/profile/update', [ProfileController::class, 'update'])->name('customer.profile.update');
+    Route::put('/customer/profile/update', [AuthRegisterController::class, 'update'])->name('customer.profile.update');
     Route::delete('/customer/profile/delete', [ProfileController::class, 'deletePhoto'])->name('delete.photo.customer');
     Route::get('/customer/map', [MapController::class, 'index'])->name('customer.map.index');
 });
