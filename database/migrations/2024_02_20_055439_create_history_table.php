57c07febdcaf8045c64ad71bb64b610ffe6a88bf
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-03-06 13:33:23 +0700

    .

--- database/migrations/2024_02_20_055439_create_history_table.php
+++ database/migrations/2024_02_20_055439_create_history_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->float('heading')->nullable();
             $table->float('speeds')->nullable();
             $table->timestamp('date_time')->nullable();
+            $table->json('original')->nullable();
             $table->timestamps();
         });
     }
