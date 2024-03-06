43f25d337147eec5fd2bd7bbfffdb5f7694bcc21
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-03-06 10:11:03 +0700

    Add location

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\HistoryController;
+use App\Http\Controllers\Api\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::apiResource('history', HistoryController::class);
+Route::get('/location', [LocationController::class, 'index']);
+Route::post('/location/store', [LocationController::class, 'store']);
