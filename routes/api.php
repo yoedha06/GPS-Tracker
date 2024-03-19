8c461f56586ba601919a762cd79e15098d764fc1
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-03-19 14:27:58 +0700

    Merge branch 'main' of https://github.com/yoedha06/GPS-Tracker into chepi

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\HistoryController; //paka a kecil
-use App\Http\Controllers\api\LocationController;
+use App\Http\Controllers\api\LocationController; //tidak terpakai
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
