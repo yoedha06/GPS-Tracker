39e9380c90b05abd5c0b43bdf3fde6cb416009bf
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-03-20 14:19:35 +0700

    .

--- app/Http/Controllers/api/HistoryController.php
+++ app/Http/Controllers/api/HistoryController.php
@@ -67,13 +67,13 @@ class HistoryController extends Controller
         $history = History::create([
             'device_id' => $device->id_device,
             'latitude' => $request->latitude,
-            // 'longitude' => $request->longitude,
+            'longitude' => $request->longitude,
             // 'bounds' => $request->bounds,
             // 'accuracy' => $request->accuracy,
-            // 'altitude' => $request->altitude,
+            'altitude' => $request->altitude,
             // 'altitude_acuracy' => $request->altitude_acuracy,
             // 'heading' => $request->heading,
-            // 'speeds' => $request->speeds,
+            'speeds' => $request->speeds,
             'date_time' => $date_time,
             'original' => json_encode($request->all())
         ]);
