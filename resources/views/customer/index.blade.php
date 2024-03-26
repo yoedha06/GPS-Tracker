35d072820c7eafd3b048bfe6558d9b3c4652f2d9
Dzaki Ahmad Fauzan <dzakiahmadfa123@gmail.com>
2024-03-26 07:41:19 +0700

    rapihkan

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -195,8 +195,8 @@
                                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                                         <h6 class="text-muted font-semibold">
                                                             <h6 class="font-bold mb-0">
-                                                                History Device
-                                                                {{-- <h6 class="font-extrabold mb-0">{{ $history }}</h6> --}}
+                                                                History 
+                                                                <h6 class="font-extrabold mb-0">{{ $totalHistoryPerDevice }}</h6>
                                                             </h6>
                                                         </h6>
                                                     </div>
