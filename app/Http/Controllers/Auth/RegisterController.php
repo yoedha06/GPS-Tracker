10987827a89ce26c99978fd31712ebe53998e1b8
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-04-26 10:48:19 +0700

    remove register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -76,21 +76,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $email = null;
-        $phone = null;
-
-        if ($data['verification_option'] == 'email') {
-            $email = $data['email'];
-        } elseif ($data['verification_option'] == 'phone') {
-            $phone = $data['phone'];
-        }
-
         // dd($data);
         $user = User::create([
             'name' => $data['name'],
-            'email' => $email,
+            'email' => $data['email'],
             'username' => $data['username'],
-            'phone' => $phone,
+            'phone' => $data['phone'],
             'password' => Hash::make($data['password']),
             'role' => 'customer',
         ]);
