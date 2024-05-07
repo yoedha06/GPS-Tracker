76d052fcadad6adecde1cfecf09a5cd9e73a5431
chepisyahbudienbasil <chepisyahbudienbasil@gmail.com>
2024-05-07 09:54:48 +0700

    test webhook lagi 18

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,31 +10,6 @@ use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
-    // public function store(Request $request)
-    // {
-    //     $url = "https://app.japati.id/api/send-message";
-
-    //     // Mengambil data dari request yang diterima oleh webhook
-    //     $requestData = $request->all();
-
-    //     // Membangun data yang akan dikirim ke endpoint
-    //     $data = [
-    //         "gateway" => $requestData['gateway'],
-    //         "number" => $requestData['from'], // Menggunakan nomor pengirim sebagai nomor penerima
-    //         "type" => "text",
-    //         "message" => $requestData['message'],
-    //     ];
-
-    //     // Log::debug('Mengirim pesan:', ['data' => $data]);
-
-    //     // Melakukan permintaan HTTP
-    //     $response = Http::withToken('API-TOKEN-iGIXgP7hUwO08mTokHFNYSiTbn36gI7PRntwoEAUXmLbSWI6p7cXqq')
-    //         ->post($url, $data);
-
-    //     // Menulis pesan debug setelah melakukan permintaan HTTP
-    //     // Log::debug('Respon dari permintaan:', ['response' => $response->getBody()->getContents()]);
-    // }
-
     public function store(Request $request)
     {
         $url = "https://app.japati.id/api/send-message";
